720f449f581315d89eee7ecf7319103aafb5dd2e
Rômulo Farias <romulodefarias@gmail.com>
2017-03-11 18:17:37 -0300

    Add 'graphical' representation of the processes

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
  *
  *  Created on: 2017-03-08
  *      Author: romulo-farias
+ *
+ *  @TODO [Bug fix] it isn't modifying the memory address in the elegible_processes array;
  */
 
 #include <stdio.h>
@@ -10,19 +12,116 @@
 
 #include "process.h"
 
-int main ()
+#define MAXIMUM_TIME_IN_SAME_PROCESS 2
+#define FILE_PATH "/home/romulo-farias/Documents/development/c/20170308_so_alghorithms/data.txt"
+
+Process* get_highest_priority(Process **processes, int items_count);
+
+int main()
 {
+	int i;
+	int times;
+	int times_in_same_process;
+	int elegible_processes_count;
+	FILE *file;
 	Process *process;
-    char *file_to_read = "/home/romulo-farias/Documents/development/c/20170308_so_alghorithms/data.txt";
-    Process **processes = read_processes_from_file(file_to_read);
+	Process *last_executed;
+	Process *executing;
+	Process **processes;
+	Process **elegible_processes;
+	file = fopen(FILE_PATH, "r");
 
-    int items_count = 4; //sizeof(Processes) / sizeof(Processes[0]);
+	if (!file)
+		return -1;
 
-    for (int i = 0; i < items_count; i++)
+	int items_count = count_lines(file);
+	processes = read_processes_from_file(file);
+
+	fclose(file);
+
+	printf("time");
+	for (i = 0; i < items_count; i++)
 	{
-		process = processes[i];
-		printf("%d %d %d\n", process->start_time, process->duration, process->priority);
+		printf("\tP%d", processes[i]->id);
 	}
 
-    return 0;
+	times = 0;
+	elegible_processes = malloc(items_count * sizeof(Process));
+	times_in_same_process = MAXIMUM_TIME_IN_SAME_PROCESS;
+	while (1 == 1)
+	{
+		printf("\n% d-", times);
+
+		if (times_in_same_process >= MAXIMUM_TIME_IN_SAME_PROCESS)
+		{
+
+			elegible_processes_count = 0;
+			for (i = 0; i < items_count; i++)
+			{
+				process = processes[i];
+				if (process->start_time <= times && process->finished == 0
+						&& process != executing)
+				{
+					elegible_processes[i] = process;
+					elegible_processes_count++;
+				}
+			}
+
+			executing = get_highest_priority(elegible_processes,
+					elegible_processes_count);
+			times_in_same_process = 1;
+		}
+		else
+		{
+			times_in_same_process++;
+		}
+
+		for (i = 0; i < items_count; i++)
+		{
+			process = processes[i];
+
+			if (process->start_time <= times)
+			{
+				if (process == executing)
+				{
+					printf("\t##");
+				}
+				else
+				{
+					printf("\t--");
+				}
+			}
+			else
+			{
+				printf("\t");
+			}
+
+		}
+
+		if (times == 3)
+			break;
+
+		last_executed = executing;
+		times++;
+	}
+
+	return 0;
+}
+
+Process* get_highest_priority(Process **processes, int items_count)
+{
+	Process *highest_priority, *process;
+
+	highest_priority = processes[0];
+
+	for (int i = 1; i < items_count; i++)
+	{
+		process = processes[i];
+		if (process->priority < highest_priority->priority)
+		{
+			highest_priority = process;
+		}
+	}
+
+	return highest_priority;
 }
--- process.h
+++ process.h
@@ -9,83 +9,104 @@
 #define SPACE_CHAR_CODE 32
 #define BREAK_LINE_CHAR_CODE 10
 
-typedef struct {
+typedef struct
+{
+	int id;
 	int start_time;
 	int duration;
 	int priority;
+	int life_time;
+	int total_time;
+	int alternations;
+	int finished;
 } Process;
 
-int count_lines (FILE *file);
-Process** read_processes_from_file (const char *path);
+int count_lines(FILE *file);
+Process * create_process();
+Process** read_processes_from_file(FILE *file);
 
-Process** read_processes_from_file (const char *path)
+Process* create_process()
 {
-	int count;
-    int character;
-    int number;
-    int lines;
-    FILE *file;
-    file = fopen(path, "r");
-    Process **processes;
-
-    if (file)
-    {
-    	processes = malloc(count_lines(file) * sizeof(Process));
-    	processes[0] = malloc(sizeof(Process));
-    	count = 0;
-    	number = 0;
-    	lines = 0;
-
-    	character = getc(file);
-        while ((character = getc(file)) != EOF)
-        {
-        	if (character != SPACE_CHAR_CODE && character != BREAK_LINE_CHAR_CODE)
-        	{
-        		number = number + (character - BASE_CHAR_NUMBER);
-        		continue;
-        	}
-
-        	switch (count)
-        	{
-        		case 0:
-        			processes[lines]->start_time = number;
-        			break;
-        		case 1:
-        			processes[lines]->duration = number;
-					break;
-        		case 2:
-        			processes[lines]->priority = number;
-					break;
-        	}
-
-        	if (character == BREAK_LINE_CHAR_CODE)
-            {
-        		lines++;
-            	count = 0;
-            	number = 0;
-            	processes[lines] = malloc(sizeof(Process));
-            	continue;
-			}
-
-            number = 0;
-            count++;
-        }
-
-        fclose(file);
-    }
-
-    return processes;
+	Process *process = malloc(sizeof(Process));
+	process->id = 0;
+	process->start_time = 0;
+	process->duration = 0;
+	process->priority = 0;
+	process->life_time = 0;
+	process->total_time = 0;
+	process->alternations = 0;
+	process->finished = 0;
+	return process;
 }
 
-int count_lines (FILE *file)
+Process** read_processes_from_file(FILE *file)
+{
+	int count;
+	int character;
+	int number;
+	int lines;
+	Process **processes;
+
+	processes = malloc(count_lines(file) * sizeof(Process));
+	processes[0] = create_process();
+	processes[0]->id = 1;
+	count = 0;
+	number = 0;
+	lines = 0;
+
+	character = getc(file);
+	while ((character = getc(file)) != EOF)
+	{
+		if (character != SPACE_CHAR_CODE && character != BREAK_LINE_CHAR_CODE)
+		{
+			number = number + (character - BASE_CHAR_NUMBER);
+			continue;
+		}
+
+		switch (count)
+		{
+		case 0:
+			processes[lines]->start_time = number;
+			break;
+		case 1:
+			processes[lines]->duration = number;
+			break;
+		case 2:
+			processes[lines]->priority = number;
+			break;
+		}
+
+		if (character == BREAK_LINE_CHAR_CODE)
+		{
+			lines++;
+			count = 0;
+			number = 0;
+			processes[lines] = create_process();
+			processes[lines]->id = lines + 1;
+			continue;
+		}
+
+		number = 0;
+		count++;
+	}
+
+	rewind(file);
+
+	return processes;
+}
+
+int count_lines(FILE *file)
 {
 	int lines = 0;
 	int character;
-	while(!feof(file))
+	while (!feof(file))
 	{
-	  character = fgetc(file);
-	  if(character == BREAK_LINE_CHAR_CODE) lines++;
+		character = fgetc(file);
+		if (character == BREAK_LINE_CHAR_CODE)
+			lines++;
 	}
+
+	lines += 1;
 
 	rewind(file);
 	return lines;
